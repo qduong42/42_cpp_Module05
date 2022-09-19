743f3e3a24cdf55bab104aeec373ea2eb9b209ed
qduong42 <15814168+qduong42@users.noreply.github.com>
2022-09-19 19:47:34 +0200

    changed decrement and increment of grade

--- ex00/Bureaucrat.cpp
+++ ex00/Bureaucrat.cpp
@@ -13,9 +13,9 @@ Bureaucrat::Bureaucrat(int grade, std::string name): _grade(0), _name(name)
 	if (DEBUG)
 		std::cout << C_GREY << "Bureaucrat grade & name constructed" C_DEF << std::endl;
 	if (grade > 150)
-		throw Bureaucrat::GradeTooHighException();
-	else if (grade < 1)
 		throw Bureaucrat::GradeTooLowException();
+	else if (grade < 1)
+		throw Bureaucrat::GradeTooHighException();
 	this->_grade = grade;
 }
 
@@ -56,13 +56,13 @@ int Bureaucrat::getGrade() const {
 }
 
 void Bureaucrat::increaseGrade() {
-	if (this->_grade + 1 > 150) throw Bureaucrat::GradeTooHighException();
-	this->_grade++;
+	if (this->_grade - 1 < 1) throw Bureaucrat::GradeTooHighException();
+	this->_grade--;
 }
 
 void Bureaucrat::decreaseGrade() {
-	if (this->_grade - 1 < 1) throw Bureaucrat::GradeTooLowException();
-	this->_grade--;
+	if (this->_grade + 1 > 150) throw Bureaucrat::GradeTooLowException();
+	this->_grade++;
 }
 
 const char* Bureaucrat::GradeTooHighException::what() const throw() {
